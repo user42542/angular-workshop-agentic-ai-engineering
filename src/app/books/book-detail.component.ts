51e141664dedba2f5c738743e162a96d9137f8d4
D.P. <detail.logen-1g@icloud.com>
2026-03-05 11:14:54 +0100

    commit

--- src/app/books/book-detail.component.ts
+++ src/app/books/book-detail.component.ts
@@ -20,8 +20,7 @@ import { BookApiClient } from './book-api-client.service';
             class="h-5 w-5 mr-2 transform group-hover:-translate-x-1 transition-transform duration-200"
             fill="none"
             stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
+            viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
           </svg>
           Back to Books
